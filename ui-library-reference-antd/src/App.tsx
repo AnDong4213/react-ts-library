6937722a4612b4522e66eb197ece60c17bbadd91
赵安东 <zhaoandong4213@sina.com>
2021-10-20 15:57:14 +0800

    init project

---
+++ ui-library-reference-antd/src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
